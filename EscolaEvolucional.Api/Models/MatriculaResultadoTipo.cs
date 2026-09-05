1b3eacc1c562e6461b73053728c05bc9918fa792
Daniel Coutinho Neto <danielcoutinhoneto@outlook.com>
2026-09-05 05:15:01 -0300

    feat: adiciona contratos de matrícula

---
+++ EscolaEvolucional.Api/Models/MatriculaResultadoTipo.cs
@@ -0,0 +1,12 @@
+namespace EscolaEvolucional.Api.Models
+{
+    public enum MatriculaResultadoTipo
+    {
+        Criada = 1,
+        AlunoNaoEncontrado = 2,
+        AlunoInativo = 3,
+        TurmaNaoEncontrada = 4,
+        TurmaSemVaga = 5,
+        Duplicada = 6
+    }
+}
